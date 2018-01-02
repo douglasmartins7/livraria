38e5a59163687a72d7e904b5cb581570ccb35fb4
unknown <mtzcpd1119@DESK_M_D0-69.intra.cvc>
2018-01-02 19:38:48 -0200

    update

---
+++ estoque.rb
@@ -0,0 +1,31 @@
+class Estoque
+    
+    def initialize
+        @livros = []
+    end
+
+    #usa o princípio Tell, Don't Ask. Ele não pergunta mais nada para o objeto livro apenas manda
+    def exporta_csv 
+        @livros.each do |livro|
+            puts livros.to_csv
+        end
+    end
+
+      def mais_barato_que valor
+        #     O resultado é um novo array
+        #     somente com os elementos que,
+        #     passados para o bloco, fazem ele devolver true.
+        @livros.select do |livro|
+            livro.preco <= valor
+        end
+    end
+
+    def total
+        @livros.size
+    end
+
+    def adiciona livro
+        @livros << livro if livro
+    end
+
+end
--- livro.rb
+++ livro.rb
@@ -1,12 +1,17 @@
+
 #base de Orientação a Objetos: dados e comportamento que são relacionados ficam no mesmo lugar.
 class Livro
     attr_reader :titulo, :preco, :ano_lancamento
   
-    def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
+    def initialize titulo, preco, ano_lancamento, possui_reimpressao
         @titulo = titulo
         @ano_lancamento = ano_lancamento
-        @preco = calcula_preco(preco)
+        @preco = calcula_preco preco
         @possui_reimpressao = possui_reimpressao
+    end
+
+    def to_csv
+        "#{@titulo},#{@ano_lancamento}, #{@preco}"
     end
 
     def possui_reimpressao?
@@ -14,10 +19,16 @@ class Livro
     end
 
     private
-    #regrea de negócio
-    def calcula_preco(base)
-        if @ano_lancamento < 2000
-            base * 0.7
+    #regra de negócio
+    def calcula_preco base
+        if @ano_lancamento < 2006
+            if @possui_reimpressao
+                base * 0.9
+            else
+                base *0.95
+            end
+        elsif @ano_lancamento <=2010
+            base * 0.96
         else
             base
         end
@@ -25,14 +36,53 @@ class Livro
 
 end
 
+class Estoque
+    
+    def initialize
+        @livros = []
+    end
+
+    #usa o princípio Tell, Don't Ask. Ele não pergunta mais nada para o objeto livro apenas manda
+    def exporta_csv 
+        @livros.each do |livro|
+            puts livro.to_csv
+        end
+    end
+
+    def mais_barato_que valor
+        #     O resultado é um novo array
+        #     somente com os elementos que,
+        #     passados para o bloco, fazem ele devolver true.
+        @livros.select do |livro|
+            livro.preco <= valor
+        end
+    end
+
+    def total
+        @livros.size
+    end
+
+    def adiciona livro
+        @livros << livro if livro
+    end
+
+end
+
     livro_rails = Livro.new("Agile Web Development with Rails", 70.00, 2011, true)
+    livro_ruby =  Livro.new("Programming Ruby 1.9", 60.00, 2010, true)
+    algoritmos =  Livro.new("Algoritmos", 100, 1998, true)
+    arquitetura = Livro.new("Introdução À Arquitetura e Design de Software", 70, 2011, true)
 
-    livro_ruby = Livro.new("Programming Ruby 1.9", 60.00, 2010, true)
-   
-    livros = [livro_rails, livro_ruby]
+    livros  = [livro_rails, livro_ruby]
 
-    algoritmos = Livro.new("Algoritmos", 100, 1998, true)
+    estoque = Estoque.new
+    estoque.adiciona algoritmos
+    estoque.adiciona arquitetura
+    estoque.adiciona Livro.new("The Pragmatic Programmer", 100, 1999, true)
+    estoque.adiciona Livro.new("Programming Ruby", 100, 2004, true)
+    estoque.adiciona nil
 
+  
     def imprime_nota_fiscal livros
         livros.each do |livro|
             livro.preco = 1.00
@@ -40,16 +90,25 @@ end
         end
     end
 
-    def livro_para_newsletter livros
-        if livros.ano_lancamento < 1999
+    def livro_para_newsletter livro
+        if livro.ano_lancamento < 1999
             puts "Newsletter/Liquidacao"
-            puts livros.titulo
-            puts livros.preco
+            puts livro.titulo
+            puts livro.preco
+            puts livro.possui_reimpressao?
         end
     end
 
     #imprime_nota_fiscal livros 
-    livro_para_newsletter algoritmos 
+    #livro_para_newsletter algoritmos 
+ 
+    baratos = mais_barato_que 80
+    baratos.each do |livro|
+        puts livro.titulo
+    end
+
+    estoque.exporta_csv
+    estoque.total
   
    
    
